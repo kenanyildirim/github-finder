c699e6e9a75fa5d38729cc7fd47f0eea7565817c
kenanyildirim <kyildirim07@gmail.com>
2020-02-08 13:28:51 +0300

    users fetrched from github api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
 import Users from "./components/users/Users";
-
+import axios from "axios"
 class App extends Component {
+ state={
+   users:[],
+   loading:false
+ }
+ async componentDidMount(){
+   this.setState({
+     loading:true
+   });
+ const res= await axios.get("https://api.github.com/users");
+ this.setState({
+   users:res.data,
+  loading:false
+});
+ 
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
-
-export class UserItem extends Component {
-   
-    render() {
-        const {login,avatar_url,html_url}=this.props.user;
+import React from 'react'
+import PropTypes from 'prop-types'
+const UserItem =(props)=> {
+        const {login,avatar_url,html_url}=props.user;
         return (
             <div className="card text-center">
                <img alt="" src={avatar_url} className="round-img" style={{width:"60px"}} />
@@ -13,7 +11,8 @@ export class UserItem extends Component {
         </div>
             </div>
         )
-    }
 }
-
+UserItem.propTypes={
+    user:PropTypes.object.isRequired,
+}
 export default UserItem
